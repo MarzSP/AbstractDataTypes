0b3b6d43f032d1a8593ccdd05c1653b2cbc6c67b
marianne <mariannepeterson92@gmail.com>
2026-01-29 14:57:18 +0100

    Tests: lists and sorting

---
+++ AbstractDataTypes.Tests/SortingTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xunit;
+using AbstractDataTypes.Sorting;
+
+namespace AbstractDataTypes.Tests;
+
+public class SortingTests
+{
+    private static void AssertSorted<T>(T[] arr) where T : IComparable<T>
+    {
+        for (int i = 1; i < arr.Length; i++)
+            Assert.True(arr[i - 1].CompareTo(arr[i]) <= 0, "Array is not sorted");
+    }
+
+    [Theory]
+    [InlineData(new int[] { })]
+    [InlineData(new int[] { 1 })]
+    [InlineData(new int[] { 2, 1 })]
+    [InlineData(new int[] { 5, 3, 8, 1, 2, 9, 7 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 5, 4, 3, 2, 1 })]
+    [InlineData(new int[] { 2, 3, 2, 1, 4, 1 })]
+    public void QuickSort_Various(int[] input)
+    {
+        var sorter = new QuickSort<int>();
+        var arr = (int[])input.Clone();
+        sorter.Sort(arr);
+        AssertSorted(arr);
+    }
+
+    [Theory]
+    [InlineData(new int[] { })]
+    [InlineData(new int[] { 1 })]
+    [InlineData(new int[] { 2, 1 })]
+    [InlineData(new int[] { 5, 3, 8, 1, 2, 9, 7 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 5, 4, 3, 2, 1 })]
+    [InlineData(new int[] { 2, 3, 2, 1, 4, 1 })]
+    public void MergeSort_Various(int[] input)
+    {
+        var sorter = new MergeSort<int>();
+        var arr = (int[])input.Clone();
+        sorter.Sort(arr);
+        AssertSorted(arr);
+    }
+
+    [Theory]
+    [InlineData(new int[] { })]
+    [InlineData(new int[] { 1 })]
+    [InlineData(new int[] { 2, 1 })]
+    [InlineData(new int[] { 5, 3, 8, 1, 2, 9, 7 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 5, 4, 3, 2, 1 })]
+    [InlineData(new int[] { 2, 3, 2, 1, 4, 1 })]
+    public void InsertionSort_Various(int[] input)
+    {
+        var sorter = new InsertionSort<int>();
+        var arr = (int[])input.Clone();
+        sorter.Sort(arr);
+        AssertSorted(arr);
+    }
+
+    [Theory]
+    [InlineData(new int[] { })]
+    [InlineData(new int[] { 1 })]
+    [InlineData(new int[] { 2, 1 })]
+    [InlineData(new int[] { 5, 3, 8, 1, 2, 9, 7 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 5, 4, 3, 2, 1 })]
+    [InlineData(new int[] { 2, 3, 2, 1, 4, 1 })]
+    public void BubbleSort_Various(int[] input)
+    {
+        var sorter = new BubbleSort<int>();
+        var arr = (int[])input.Clone();
+        sorter.Sort(arr);
+        AssertSorted(arr);
+    }
+}
