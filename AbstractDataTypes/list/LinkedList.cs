eab6979c0dccc779409a95135260528f731a7e88
marianne <mariannepeterson92@gmail.com>
2026-01-29 14:42:33 +0100

    Feat: linkedlist and sortedlist implementation

--- AbstractDataTypes/list/LinkedList.cs
+++ AbstractDataTypes/list/LinkedList.cs
@@ -1,41 +1,201 @@
 ﻿namespace AbstractDataTypes.list;
-
+// Implementation of the ADT Linked List using a singly linked list
 public class LinkedList<T> : IList<T>
 {
+    private class Node
+    {
+        public T Value;
+        public Node? Next;
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    private Node? _head;
+    private int _count;
+
+    /**
+     * Adds an element to the end of the linked list
+     * TC: Best: O(1) Worst: O(n)
+     */
     public void Add(T element)
     {
-        throw new NotImplementedException();
+        Node newNode = new Node(element);
+        if (_head == null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            Node current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = newNode;
+        }
+        _count++;
     }
 
+    /**
+     * Inserts an element at the specified index
+     * TC: Best: O(1) Worst: O(n)
+     */
     public void Insert(int index, T element)
     {
-        throw new NotImplementedException();
+        if (index < 0 || index > _count)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        Node newNode = new Node(element);
+        if (index == 0)
+        {
+            newNode.Next = _head;
+            _head = newNode;
+        }
+        else
+        {
+            Node current = _head!; // index > 0 so _head is not null
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.Next!;
+            }
+
+            newNode.Next = current.Next;
+            current.Next = newNode;
+        }
+
+        _count++;
     }
 
+    /**
+     * This is an indexer: which means you can access elements like array[index]
+     * TC: Best: O(1) Worst: O(n)
+     */
     public T this[int index]
     {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
+        get
+        {
+            if (index < 0 || index >= _count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            Node current = _head!;
+            for (var i = 0; i < index; i++)
+                current = current.Next!;
+
+            return current.Value;
+        }
+        set
+        {
+            if (index < 0 || index >= _count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            Node current = _head!;
+            for (var i = 0; i < index; i++)
+                current = current.Next!;
+
+            current.Value = value;
+        }
     }
 
+    /**
+     * Removes the element at the specified index
+     * TC: Best: O(1) Worst: O(n)
+     */
     public void RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        if (index == 0)
+        {
+            _head = _head?.Next;
+        }
+        else
+        {
+            Node current = _head!;
+            for (var i = 0; i < index - 1; i++)
+                current = current.Next!;
+
+            // current is node before the one we want to remove
+            current.Next = current.Next?.Next;
+        }
+
+        _count--;
     }
 
+    /**
+     * Removes the first occurrence of the specified element
+     * TC: Best: O(1) Worst: O(n)
+     */
     public bool Remove(T element)
     {
-        throw new NotImplementedException();
+        Node? current = _head;
+        Node? previous = null;
+
+        EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+
+        while (current != null)
+        {
+            if (comparer.Equals(current.Value, element))
+            {
+                if (previous == null)
+                {
+                    // removing head
+                    _head = current.Next;
+                }
+                else
+                {
+                    previous.Next = current.Next;
+                }
+
+                _count--;
+                return true;
+            }
+
+            previous = current;
+            current = current.Next;
+        }
+
+        return false;
     }
 
-    public int Count { get; }
+    /**
+     * This property returns the number of elements in the linked list
+     * This notation is shorthand for a getter method
+     * TC: O(1)
+     */
+    public int Count => _count;
+
+    /**
+     * Checks if the linked list contains the specified element
+     * TC: Best: O(1) Worst: O(n)
+     */
     public bool Contains(T element)
     {
-        throw new NotImplementedException();
+        Node? current = _head;
+        EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+
+        while (current != null)
+        {
+            if (comparer.Equals(current.Value, element))
+                return true;
+            current = current.Next;
+        }
+
+        return false;
     }
 
+    /**
+     * Clears the linked list
+     * TC: O(1)
+     */
     public void Clear()
     {
-        throw new NotImplementedException();
+        _head = null;
+        _count = 0;
     }
 }
--- AbstractDataTypes/list/SortedList.cs
+++ AbstractDataTypes/list/SortedList.cs
@@ -1,41 +1,95 @@
-﻿namespace AbstractDataTypes.list;
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractDataTypes.list;
 
 public class SortedList<T> : IList<T>
 {
+    private readonly List<T> _items;
+    private readonly IComparer<T> _comparer;
+
+    /** Creates an empty SortedList using the default comparer
+     * TC: Best: O(1) Worst: O(1) */
+    public SortedList() : this(null)
+    {
+    }
+
+    /** Creates an empty SortedList using the provided comparer
+     * TC: Best: O(1) Worst: O(1) */
+    public SortedList(IComparer<T>? comparer)
+    {
+        _comparer = comparer ?? Comparer<T>.Default;
+        _items = new List<T>();
+    }
+
+    /** Adds an element while keeping the list sorted (inserts at proper index)
+     * TC: Best: O(1) Worst: O(n) */
     public void Add(T element)
     {
-        throw new NotImplementedException();
+        var idx = _items.BinarySearch(element, _comparer);
+        if (idx < 0) idx = ~idx;
+        _items.Insert(idx, element);
     }
 
+    /** Inserts an element; for SortedList the index parameter is ignored and element is inserted in its sorted position.
+     * TC: Best: O(1) Worst: O(n) */
     public void Insert(int index, T element)
     {
-        throw new NotImplementedException();
+        // Validate index range to match IList contract, but still insert by value
+        if (index < 0 || index > _items.Count)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        Add(element);
     }
 
+    /** Gets or sets the element at the specified index. Setting reinserts the value to keep order.
+     * TC: Best: O(1) Worst: O(n) */
     public T this[int index]
     {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
+        get => _items[index];
+        set
+        {
+            if (index < 0 || index >= _items.Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            // Remove the item at index and re-insert the new value in sorted position
+            _items.RemoveAt(index);
+            Add(value);
+        }
     }
 
+    /** Removes the element at the given index
+     * TC: Best: O(1) Worst: O(n) */
     public void RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        _items.RemoveAt(index);
     }
 
+    /** Removes the first occurrence of element (if present)
+     * TC: Best: O(1) Worst: O(n) */
     public bool Remove(T element)
     {
-        throw new NotImplementedException();
+        var idx = _items.BinarySearch(element, _comparer);
+        if (idx < 0) return false;
+        _items.RemoveAt(idx);
+        return true;
     }
 
-    public int Count { get; }
+    /** Number of elements in the list
+     * TC: Best: O(1) Worst: O(1) */
+    public int Count => _items.Count;
+
+    /** Returns true if element is present (uses binary search)
+     * TC: Best: O(1) Worst: O(log n) */
     public bool Contains(T element)
     {
-        throw new NotImplementedException();
+        return _items.BinarySearch(element, _comparer) >= 0;
     }
 
+    /** Clears the list
+     * TC: Best: O(1) Worst: O(n) */
     public void Clear()
     {
-        throw new NotImplementedException();
+        _items.Clear();
     }
 }
